7020e071358f9eecaf63d791a1b43edf88150f38
naresh26git <111833906+naresh26git@users.noreply.github.com>
2023-06-14 22:44:49 +0530

    Update App.jsx

--- ui-web-app-reactjs/src/components/App.jsx
+++ ui-web-app-reactjs/src/components/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
 
   const handleApiCall = async (e) => {
     // const url = 'http://localhost:9999/'+e.target.name;
-    let url = 'http://13.233.250.233:32470/'+e.target.name
+    let url = 'http://65.0.3.115:32470/'+e.target.name
 
     displayData.url=url
     let options = {}
